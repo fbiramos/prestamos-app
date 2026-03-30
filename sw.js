9bff0d51227e27feb8da844cfe267ed3f620b1fb
Fabian <fabiojuanramoszapana@gmail.com>
2026-03-30 18:57:18 -0400

    Corrección de SW v27 y preparación para sincronización de datos

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'prestamos-cache-v25';
+const CACHE_NAME = 'prestamos-cache-v27';
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
-  '/app.js?v=25',
+  '/app.js?v=27',
   '/manifest.json?v=4',
   'https://cdn.tailwindcss.com',
   '/icons/icon-192x192.png?v=4',
