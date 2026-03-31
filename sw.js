1b7da13b30a279038c19ef7bd7e521bb769a4ee8
Fabian <fabiojuanramoszapana@gmail.com>
2026-03-30 20:44:13 -0400

    Aumentado tamaño de fuentes en resumen v37

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = 'prestamos-cache-v36';
+const CACHE_NAME = 'prestamos-cache-v37';
 const urlsToCache = [
   '/',
   '/index.html',
   '/style.css',
-  '/app.js?v=36',
+  '/app.js?v=37',
   '/manifest.json?v=4',
   'https://cdn.tailwindcss.com',
   '/icons/icon-192x192.png?v=4',
